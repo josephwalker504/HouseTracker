0449e638e32658d4823b77bcaaa908d23d57ea28
joseph walker <josephwalker504@gmail.com>
2020-03-24 17:11:45 -0500

    edited code for adding function on neighborhood views

--- HouseTracker/trackerapp/views/neighborhoods/neighborhoodform.py
+++ HouseTracker/trackerapp/views/neighborhoods/neighborhoodform.py
@@ -2,7 +2,7 @@ import sqlite3
 from django.shortcuts import render, redirect, reverse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from trackerapp.models import model_factory
+# from trackerapp.models import model_factory
 from ..connection import Connection
 from trackerapp.models import Neighborhood
 
@@ -10,36 +10,34 @@ from trackerapp.models import Neighborhood
 
 
 
-def get_neighborhoods():
-    with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = model_factory(Neighborhood)
-        db_cursor = conn.cursor()
+# def get_neighborhoods():
+#     with sqlite3.connect(Connection.db_path) as conn:
+#         conn.row_factory = model_factory(Neighborhood)
+#         db_cursor = conn.cursor()
 
-        db_cursor.execute("""
-        select
-            n.id,
-            n.name
-        from trackerapp_neighborhood n 
-        """)
+#         db_cursor.execute("""
+#         select
+#             n.id,
+#             n.name
+#         from trackerapp_neighborhood n 
+#         """)
 
-        return db_cursor.fetchall()
+#         return db_cursor.fetchall()
 
 
 @login_required
 def neighborhood_form(request):
     if request.method == 'GET':
-        neighborhoods = get_neighborhoods()
+        neighborhoods = Neighborhood.objects.all()
         template = 'neighborhoods/form.html'
-        context = {
-            'neighborhoods': neighborhoods
-        }
-        return render(request, template, context)
+        
+        return render(request, template,)
 
 @login_required
 def neighborhood_edit_form(request, neighborhood_id):
 
     if request.method == 'GET':
-        neighborhood = get_neighborhoods(neighborhood_id)
+        neighborhood = Neighborhood.objects.get(neighborhood_id)
         template = 'neighborhoods/form.html'
         context = {
             'neighborhood': neighborhood,
